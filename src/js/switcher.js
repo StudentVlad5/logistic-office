28214221459ef4c237bc540e77bafe3e650bbd95
Heorgii <103379960+Heorgii@users.noreply.github.com>
2023-12-19 10:39:08 +0200

    fix

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -108,3 +108,153 @@ btnBack.addEventListener('click', function () {
 btnNext.addEventListener('click', function () {
   changeText(1);
 });
+
+// last bord
+
+const lastTitleArr1 = [
+  'Накладна на вантаж',
+  'Персональні дані Водія',
+  'Досвід та надійність',
+  'Захист від "накручування"',
+  'Відстежування',
+  'Документація',
+  'Вирішення суперечок',
+];
+
+const lastDiscrArr1 = [
+  'Ніколи не передавайте вантаж Водію без оформлення накладної. Не наражайте себе на зайвий ризик. Електронна накладна автоматично формується сервісом та зберігається в базі документів.',
+  'Адміністрація сервісу проводить премодерацію документів, наданих Водієм. Відправник завжди має можливість порівняти персональні дані Водія і дані автомобіля, зазначені в накладній і фактичні.',
+  'Рейтинг Водія, а також залишені про нього відгуки, дають уявлення про його досвід та ступінь надійності',
+  'Щоб уникнути накруток рейтингу Водієм, можливість отримувати його є тільки в разі, якщо факт доставки вантажу у вказане місце зафіксований сервісом.',
+  'Відправник і Одержувач вантажу мають можливість стежити за пересуванням вантажу онлайн.',
+  'Адміністрація сайту гарантує зберігання електронних копій накладних та Договорів, укладених між користувачами',
+  'При виникненні спірних ситуацій з Водієм після створення накладної (накладна створюється сервісом автоматично після згоди Відправника на доставку вантажу Водієм), передбачена можливість відкриття спору Відправником або Одержувачем в особистому кабінеті із залученням або без залучення представника Адміністрації. ',
+];
+
+const lastTitleArr2 = [
+  'Накладна на груз',
+  'Довіра',
+  'Рейтинг',
+  'Відстежування',
+  "Зворотній з'вязок",
+  'Допомога',
+  'Вирішення суперечок',
+];
+
+const lastDiscrArr2 = [
+  'Ніколи не доставляйте вантаж без оформлення накладної. Не беріть він зайвої відповідальності. Електронна накладна автоматично формується сервісом та зберігається в базі документів.',
+  "Для підвищення довіри Клієнтів Адміністрація проводить премодерацію документів, наданих при реєстрації. Таккже згідно з правилами сервісу доставка вантажів повинна здійснюватися з активним з'єднанням онлайн відстеження.",
+  'Можливість отримувати рейтинг є тільки в разі підтвердження сервісом фактичної доставки вантажу в зазначене у накладній місце.',
+  'Відстежування працює тільки під час виконання Маршруту та відключається автоматично після виконання усіх Замовлень по даному Маршруту',
+  'Водій, також як і Клієнт, має можливість залишати відгуки',
+  'У разі виникнення непередбачених ситуацій Водій завжди може скористатися екстреною допомогою в дорозі, натиснувши на відповідну кнопку в накладній. Залежно від типу звернення його запит про допомогу отримає Адміністрація або інші Водії, які їдуть в попутному напрямку.',
+  'При виникненні спірних ситуацій з Відправником або Одержувачем після створення накладної (накладна створюється сервісом автоматично після згоди Клієнта на доставку вантажу Водієм), передбачена можливість відкриття спору в особистому кабінеті із залученням або без залучення представника Адміністрації. ',
+];
+
+const lastWord1 = document.querySelectorAll('.last-word1');
+const lastWord2 = document.querySelectorAll('.last-word2');
+const lastSwitcher = document.querySelectorAll('.last-switcher');
+const lastSwitchText = document.getElementById('last-switch-title');
+const lastSwitchDiscr = document.getElementById('last-switch-discr');
+const lastCount = document.querySelector('.num_box-num-text');
+const lastBtnBack = document.querySelector('.last-btn-back');
+const lastBtnNext = document.querySelector('.last-btn-next');
+
+let lastCurrentIndex = 0;
+
+lastWord1.forEach(el => {
+  el.addEventListener('click', function () {
+    lastSwitcher.forEach(it => (it.checked = false));
+    el.classList.add('label--isActive2');
+    lastWord2.forEach(otherEl => otherEl.classList.remove('label--isActive2'));
+
+    lastCurrentIndex = 0;
+    lastSwitchText.textContent = lastTitleArr2[lastCurrentIndex];
+    lastSwitchDiscr.textContent = lastDiscrArr2[lastCurrentIndex];
+    lastCount.textContent = `0${lastCurrentIndex + 1}`;
+  });
+});
+
+lastWord2.forEach(el => {
+  el.addEventListener('click', function () {
+    lastSwitcher.forEach(it => (it.checked = true));
+    el.classList.add('label--isActive2');
+    lastWord1.forEach(otherEl => otherEl.classList.remove('label--isActive2'));
+
+    lastCurrentIndex = 0;
+    lastSwitchText.textContent = lastTitleArr1[lastCurrentIndex];
+    lastSwitchDiscr.textContent = lastDiscrArr1[lastCurrentIndex];
+
+    lastCount.textContent = `0${lastCurrentIndex + 1}`;
+  });
+});
+
+lastSwitcher.forEach(el => {
+  el.addEventListener('click', function () {
+    if (el.checked == true) {
+      lastWord2.forEach(otherEl => otherEl.classList.add('label--isActive2'));
+      lastWord1.forEach(otherEl => otherEl.classList.remove('label--isActive2'));
+
+      lastCurrentIndex = 0;
+      lastSwitchText.textContent = lastTitleArr1[lastCurrentIndex];
+      lastSwitchDiscr.textContent = lastDiscrArr1[lastCurrentIndex];
+      lastCount.textContent = `0${lastCurrentIndex + 1}`;
+    } else {
+      lastWord2.forEach(otherEl => otherEl.classList.remove('label--isActive2'));
+      lastWord1.forEach(otherEl => otherEl.classList.add('label--isActive2'));
+
+      lastCurrentIndex = 0;
+      lastSwitchText.textContent = lastTitleArr2[lastCurrentIndex];
+      lastSwitchDiscr.textContent = lastDiscrArr2[lastCurrentIndex];
+
+      lastCount.textContent = `0${lastCurrentIndex + 1}`;
+    }
+  });
+});
+
+lastSwitcher.forEach(el => {
+  el.addEventListener('change', function () {
+    lastCurrentIndex = 0;
+    changeLastText(0);
+  });
+});
+
+function changeLastText(step) {
+  lastCurrentIndex += step;
+
+  if (lastCurrentIndex < 0) {
+    lastCurrentIndex = lastSwitcher[0].checked
+      ? lastTitleArr1.length - 1
+      : lastTitleArr2.length - 1;
+  } else if (
+    lastCurrentIndex >=
+    (lastSwitcher[0].checked
+      ? lastTitleArr2.length
+      : lastTitleArr1.length)
+  ) {
+    lastCurrentIndex = 0;
+  }
+
+  const activeTitleArr = lastSwitcher[0].checked
+    ? lastTitleArr2
+    : lastTitleArr1;
+
+  const activeDiscrArr = lastSwitcher[0].checked
+    ? lastDiscrArr2
+    : lastDiscrArr1;
+
+  lastSwitchText.textContent = activeTitleArr[lastCurrentIndex];
+  lastSwitchDiscr.textContent = activeDiscrArr[lastCurrentIndex];
+
+  lastCount.textContent = `0${lastCurrentIndex + 1}`;
+}
+
+changeLastText(0);
+
+lastBtnBack.addEventListener('click', function () {
+  changeLastText(-1);
+});
+
+lastBtnNext.addEventListener('click', function () {
+  changeLastText(1);
+});
